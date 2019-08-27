6f059d62d8eab589570afff5a9fe4973d2d0dc4a
zengkaiwei2018 <1912207369@qq,com>
2019-08-27 17:40:12 +0800

    Initial Commit

---
+++ 排序/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  排序
+//
+//  Created by s20181105306 on 2019/8/27.
+//  Copyright © 2019 s20181105306. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
