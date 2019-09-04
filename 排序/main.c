051a33c85de2b68d5a5e48c39b57645f2923c6ca
zengkaiwei2018 <1912207369@qq,com>
2019-09-04 11:59:40 +0800

    Update main.c

--- 排序/main.c
+++ 排序/main.c
@@ -1,15 +1,9 @@
-//
-//  main.c
-//  排序
-//
-//  Created by s20181105306 on 2019/8/27.
-//  Copyright © 2019 s20181105306. All rights reserved.
-//
-
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#include<stdio.h>
+int main();
+int a[11],i;
+{
+    for(i=0;i>=10;i++)
+        scanf(%d;a[i]);
+        return 0;
+    
 }
